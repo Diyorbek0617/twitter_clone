3196a2c23c5a0f3d60a86dd413269374c7e3c63e
Diyorbek0617 <nizomiddinov.d.d@gmail.com>
2022-05-12 09:09:50 +0500

    Splash page navigated two second other page.

--- app/src/main/java/com/fam/twitter_clone/SplashActivity.java
+++ app/src/main/java/com/fam/twitter_clone/SplashActivity.java
@@ -1,14 +1,25 @@
 package com.fam.twitter_clone;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
+    @SuppressLint("Range")
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        ImageView  vl_note=findViewById(R.id.iv_note);
+        vl_note.animate().setDuration(2000).alpha(2f).withStartAction(() -> {
+            Intent i=new Intent(this,MainActivity.class);
+            startActivity(i);
+            //overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+        finish();
+        });
     }
 }
