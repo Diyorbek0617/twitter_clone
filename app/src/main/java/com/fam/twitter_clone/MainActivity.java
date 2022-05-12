68537199c649b0a67c5df9dd4a6f4d2fac5956d7
Diyorbek0617 <nizomiddinov.d.d@gmail.com>
2022-05-12 08:43:00 +0500

    navigated next page.

--- app/src/main/java/com/fam/twitter_clone/MainActivity.java
+++ app/src/main/java/com/fam/twitter_clone/MainActivity.java
@@ -2,7 +2,10 @@ package com.fam.twitter_clone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initviews();
+    }
+    void initviews(){
+        Button button_click=findViewById(R.id.button_1);
+        button_click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                click1();
+            }
+        });
+    }
+    void click1(){
+        Intent intent= new Intent(this,DetailActivity.class);
+        startActivity(intent);
     }
 }
